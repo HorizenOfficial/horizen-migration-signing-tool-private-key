155bafeda3acfc809bd7acfdf289c113d5a107c6
Jofra Montesdeoca <jmontesdeocanuez21@gmail.com>
2025-01-20 13:06:46 +0000

    refactor: Disable optimization and compression

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   assetPrefix: "./",
-  
+  compress: false,
+  webpack(webpackConfig) {
+    return {
+      ...webpackConfig,
+      optimization: {
+        minimize: false,
+      },
+    };
+  },
 };
 
 export default nextConfig;
