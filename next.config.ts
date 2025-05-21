cf028715a85f227f3f31150bf99939177a426f43
Yuriko Inaba <yurikoainaba@gmail.com>
2025-05-21 09:10:35 -0400

    Add security headers

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,47 @@ const nextConfig: NextConfig = {
       },
     };
   },
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Referrer-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "strict-transport-security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "x-content-type-options",
+            value: "nosniff",
+          },
+          {
+            key: "x-frame-options",
+            value: "DENY",
+          },
+          {
+            key: "x-xss-protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self';
+              object-src 'none';
+              base-uri 'self';
+              frame-ancestors 'none';
+            `
+              .replace(/\s{2,}/g, " ")
+              .trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
